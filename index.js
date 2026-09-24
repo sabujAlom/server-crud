8d4b481ef2c64f64c41072d353e8388ac91a13fb
sabujAlom <sabujalom18@gmail.com>
2026-09-24 08:09:57 +0600

    conected to mongodb

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ app.use(cors())
 
 
 
+const { MongoClient } =require('mongodb');
+
+const client = new MongoClient(process.env.DB_URI);
+
+ async function connectToMongoDB() {
+  try {
+    await client.connect();
+
+
+
+
+
+    console.log("You successfully connected to MongoDB!");
+    return client;
+  } catch (err) {
+    console.dir(err);
+  }
+}
+connectToMongoDB()
+
+
+
+
 
 
 app.get('/', (req, res) => {
