9834f3729e74facf99c5d7531d416761cd6569aa
sabujAlom <sabujalom18@gmail.com>
2026-09-24 08:51:15 +0600

    get all product and single product

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ app.use(cors())
 
 
 
-const { MongoClient } =require('mongodb');
+const { MongoClient, ObjectId } =require('mongodb');
 
 const client = new MongoClient(process.env.DB_URI);
 
  async function connectToMongoDB() {
   try {
     await client.connect();
+    const db = client.db("cars")
+    const productsCollection = db.collection("products")
 
+    app.get("/products", async(req, res)=>{
+        const result = await productsCollection.find().toArray()
+         res.send(result);
+    })
+
+    app.get("/products/:productId", async(req, res)=>{
+        const productId =req.params.productId
+        const query = {_id:new ObjectId(productId)}
+        
+        const result = await productsCollection.findOne(query);
+        res.send(result);
+    })
 
 
 
